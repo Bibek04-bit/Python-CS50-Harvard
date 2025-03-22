c5e37c9a1ca4e29318dd13d0cd38d8eb2b9ab4e9
Bibek Acharya <bibekacharya2059@gmail.com>
2025-03-22 13:22:43 +0545

    dictonaries in python

---
+++ dictonaries.py
@@ -0,0 +1,18 @@
+students = {'name': 'Bibek', 'age': 25, 'courses': ['math', 'computer']}
+#students.update({'name': 'hari', 'age':22, 'phone':9841222})
+#print(students)
+#print(students['courses'])
+#print(students['name'])
+#print(students.get('phone', 'not found'))
+#del students['age']
+#age = students.pop('age')
+#print(age)
+#print(len(students))
+#print(students.keys())
+#print(students.values())
+#print(students)
+#for key in students:
+ # print(key)
+'''for key, value in students.items():
+  print(key, value)'''
+
