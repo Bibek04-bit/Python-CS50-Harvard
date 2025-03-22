8d062fd64a37121acda8db1ab7a5040b30bdf292
Bibek Acharya <bibekacharya2059@gmail.com>
2025-03-22 13:23:01 +0545

    list, tuple and set

---
+++ listtuplesset.py
@@ -0,0 +1,21 @@
+"""friends = ['kabindra','jatan','sulav','amit']
+print(friends)"""
+
+
+
+"""friends = ['kabindra','jatan','sulav','amit']
+print(friends[-4])"""
+
+"""friends = ['kabindra','jatan','sulav','amit']
+print(friends[0:2])
+#the beginning is inclusive but the end is exclusive remember"""
+
+"""friends = ['kabindra','jatan','sulav','amit']
+friends.append('bibek')
+print(friends)"""
+
+
+friends = ['kabindra','jatan','sulav','amit']
+friends.insert(0, 'bibek')
+print(friends)
+
