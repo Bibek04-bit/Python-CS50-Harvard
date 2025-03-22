961811f0bca54781c86af59698918ff4b3a925bb
Bibek Acharya <bibekacharya2059@gmail.com>
2025-03-22 13:22:27 +0545

    conditionals in python

---
+++ conditionals.py
@@ -0,0 +1,22 @@
+'''booleans are true or false values'''
+#if True:
+ # print('bibek')
+#if False:
+# print('nothing')
+#name = 'Bibek'
+#if name == 'Bibek':
+# print('Bibek was here')
+#else:
+# print('Bibek was not here')
+'''x=0
+while x<5:
+  print(x)
+  x+=1'''
+'''language = input(str('language: ' ))
+if language == 'python':
+  print('there is a match')
+else:
+  print('no match')'''
+
+
+
